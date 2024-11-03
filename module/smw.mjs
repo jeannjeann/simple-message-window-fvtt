1e2855063537520048eb971006003c73923bcf9c
Jean.N <136861083+jeannjeann@users.noreply.github.com>
2024-11-03 19:31:57 +0900

    settings about each size

--- module/smw.mjs
+++ module/smw.mjs
@@ -56,6 +56,62 @@ Hooks.once("init", () => {
     type: Boolean,
     default: false,
   });
+  game.settings.register("simple-message-window", "fontSize", {
+    name: game.i18n.localize("SETTING.fontSize.name"),
+    hint: game.i18n.localize("SETTING.fontSize.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 100,
+  });
+  game.settings.register("simple-message-window", "textSpeed", {
+    name: game.i18n.localize("SETTING.textSpeed.name"),
+    hint: game.i18n.localize("SETTING.textSpeed.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 100,
+  });
+  game.settings.register("simple-message-window", "windowPosition", {
+    name: game.i18n.localize("SETTING.windowPosition.name"),
+    hint: game.i18n.localize("SETTING.windowPosition.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 5,
+  });
+  game.settings.register("simple-message-window", "windowWidth", {
+    name: game.i18n.localize("SETTING.windowWidth.name"),
+    hint: game.i18n.localize("SETTING.windowWidth.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 100,
+  });
+  game.settings.register("simple-message-window", "windowHeight", {
+    name: game.i18n.localize("SETTING.windowHeight.name"),
+    hint: game.i18n.localize("SETTING.windowHeight.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 100,
+  });
+  game.settings.register("simple-message-window", "showImg", {
+    name: game.i18n.localize("SETTING.showImg.name"),
+    hint: game.i18n.localize("SETTING.showImg.hint"),
+    scope: "client",
+    config: true,
+    type: Boolean,
+    default: true,
+  });
+  game.settings.register("simple-message-window", "imgSize", {
+    name: game.i18n.localize("SETTING.imgSize.name"),
+    hint: game.i18n.localize("SETTING.imgSize.hint"),
+    scope: "client",
+    config: true,
+    type: Number,
+    default: 100,
+  });
 });
 
 Hooks.once("ready", () => {
@@ -157,8 +213,18 @@ function adjustOverlaySize() {
     const sidebarWidth = sidebar.offsetWidth;
     const overlayWidth = boardWidth - sidebarWidth;
 
-    const chatWidth = overlayWidth * 0.7;
-    const chatHeight = overlayWidth * 0.15;
+    // window position
+    let windowPositionSetting =
+      game.settings.get("simple-message-window", "windowPosition") ?? 5;
+    chatOverlay.style.bottom = `${windowPositionSetting}%`;
+
+    // window size
+    let windowWidthSetting =
+      game.settings.get("simple-message-window", "windowWidth") ?? 100;
+    let windowHeightSetting =
+      game.settings.get("simple-message-window", "windowHeight") ?? 100;
+    const chatWidth = (overlayWidth * 0.7 * windowWidthSetting) / 100;
+    const chatHeight = (overlayWidth * 0.15 * windowHeightSetting) / 100;
     const chatLeft = overlayWidth / 2;
     const chatBottom =
       parseFloat(window.getComputedStyle(chatOverlay).bottom) || 0;
@@ -176,8 +242,17 @@ function adjustOverlaySize() {
       messageElement.style.maxHeight = `${contentHeight}px`;
     }
 
-    const portraitHeigt = chatHeight;
-    const portraitWidth = chatHeight;
+    // font size
+    let fontSizeSetting =
+      game.settings.get("simple-message-window", "fontSize") ?? 100;
+    const fontSize = (1.2 * fontSizeSetting) / 100;
+    chatOverlay.style.setProperty("font-size", `${fontSize}em`);
+
+    // portrait size
+    let imgSizeSetting =
+      game.settings.get("simple-message-window", "imgSize") ?? 100;
+    const portraitHeigt = (chatHeight * imgSizeSetting) / 100;
+    const portraitWidth = (chatHeight * imgSizeSetting) / 100;
     const portraitLeft = chatLeft - chatWidth / 2 + portraitWidth / 2 + 10;
     const portraitBottom = chatBottom + chatHeight;
 
@@ -192,9 +267,9 @@ function adjustOverlaySize() {
 async function showMessage() {
   if (messageQueue.length == 0) return;
   isTyping = true;
-
   const chatOverlay = document.getElementById("smw-chat-overlay");
   const portraitOverlay = document.getElementById("smw-portrait-overlay");
+
   if (!chatOverlay) return;
 
   const message = messageQueue.shift();
@@ -204,7 +279,11 @@ async function showMessage() {
   const token = game.tokens?.get(message.speaker.token) || null;
   const characterImg = actor?.img;
   const playerImg = message.user.avatar;
-  const textspeed = 80;
+
+  // text Speed
+  let textSpeedSetting =
+    game.settings.get("simple-message-window", "textSpeed") ?? 100;
+  const textspeed = 80 / (textSpeedSetting / 100);
 
   const window = await renderTemplate(
     "modules/simple-message-window/templates/smw.hbs",
@@ -222,7 +301,12 @@ async function showMessage() {
   }
 
   chatOverlay.style.display = "block";
-  portraitOverlay.style.display = "block";
+  let showImg = game.settings.get("simple-message-window", "showImg");
+  if (showImg) {
+    portraitOverlay.style.display = "block";
+  } else {
+    portraitOverlay.style.display = "none";
+  }
   adjustOverlaySize();
 
   let skip = false;
